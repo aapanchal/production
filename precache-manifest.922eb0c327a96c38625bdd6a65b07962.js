14b4bfc8548a5cf299977e04f67d5c35c63d110f
Aniket Panchal <aniketpanchal.5097@gmail.com>
2020-04-25 15:46:35 +0530

    Updates

---
+++ precache-manifest.922eb0c327a96c38625bdd6a65b07962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fb82786767379cc6a44737d8d86116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77379cc3a75908254638",
+    "url": "/static/css/main.77fbf7b3.chunk.css"
+  },
+  {
+    "revision": "9a8176d3dbe5c3278bc4",
+    "url": "/static/js/2.502dc4d2.chunk.js"
+  },
+  {
+    "revision": "bec6065eb930b261fb6e8dd38a11681f",
+    "url": "/static/js/2.502dc4d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77379cc3a75908254638",
+    "url": "/static/js/main.01c16623.chunk.js"
+  },
+  {
+    "revision": "d35f2f7cea8399bd5e1c",
+    "url": "/static/js/runtime-main.de7f2671.js"
+  }
+]);
